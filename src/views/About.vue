322af303be817d63975436a04fa021d8bbf085b1
kevinhes <kevinhes1115@gmail.com>
2022-07-21 13:05:37 +0800

    關於我的圖片

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,8 +1,14 @@
 <template>
-  <div class="container">
-    <h1>This is an about page</h1>
-    <a href="#" class="btn btn-primary btn-lg me-3">try</a>
-    <a href="#" class="btn btn-primary btn-lg me-3">try</a>
-    <a href="#" class="btn btn-primary btn-lg">try</a>
+  <div class="container-fluid min-vh-100" style="background-image: url(https://images.unsplash.com/photo-1506749841726-3df2e72b9564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80)">
+    <div class="conatiner">
+      <div class="row">
+        <div class="offset-1 col-6">
+          <h1>This is an about page</h1>
+          <a href="#" class="btn btn-primary btn-lg me-3">try</a>
+          <a href="#" class="btn btn-primary btn-lg me-3">try</a>
+          <a href="#" class="btn btn-primary btn-lg">try</a>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
